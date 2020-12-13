a43fa69ecd9ec125ec5e0d94738d0cf2d4f74449
jessebrite <jessebrite@gmail.com>
2020-12-13 19:01:08 +0000

    POST request for Design and Order controllers added

--- src/main/java/tacos/DesignTacoController.java
+++ src/main/java/tacos/DesignTacoController.java
@@ -3,15 +3,16 @@ package tacos;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.Ingredient.Type;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import tacos.Ingredient;
-import tacos.Ingredient.Type;
 
 //import javax.validation.Valid;
 
@@ -46,7 +47,20 @@ public class DesignTacoController {
 		return "design";
 	}
 
-	private Object filterByType(List<Ingredient> ingredients, Type type) {
+	@PostMapping
+	public String processDesign(@Valid Taco design, Errors errors) {
+		if (errors.hasErrors())
+			return "design";
+
+		// Save the taco design...
+		// We'll do this in later
+		log.info("Process design: " + design);
+
+		return "redirect:/orders/current";
+	}
+
+	private List<Ingredient> filterByType(
+			List<Ingredient> ingredients, Type type) {
 		return ingredients.stream()
 			.filter(x -> x.getType().equals(type))
 			.collect(Collectors.toList());
