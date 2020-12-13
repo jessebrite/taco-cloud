9322b0b3781c52f87a4dde83b529940c26772485
jessebrite <jessebrite@gmail.com>
2020-12-13 22:34:14 +0000

    @ModelAttribute annotation added to post method in Design controller to display errors in view

--- src/main/java/tacos/DesignTacoController.java
+++ src/main/java/tacos/DesignTacoController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient.Type;
@@ -48,14 +49,14 @@ public class DesignTacoController {
 	}
 
 	@PostMapping
-	public String processDesign(@Valid Taco design, Errors errors) {
+	public String processDesign(
+		@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
 		if (errors.hasErrors())
 			return "design";
 
 		// Save the taco design...
 		// We'll do this in later
 		log.info("Process design: " + design);
-
 		return "redirect:/orders/current";
 	}
 
