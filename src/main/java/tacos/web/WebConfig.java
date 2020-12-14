c20fd5a1e8ebff75e985a610026b962da6c5ab94
jessebrite <jessebrite@gmail.com>
2020-12-14 23:25:39 +0000

    prepopulated data added

--- src/main/java/tacos/web/WebConfig.java
+++ src/main/java/tacos/web/WebConfig.java
@@ -1,8 +1,6 @@
 package tacos.web;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
