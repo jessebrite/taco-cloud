095f57d2d63574fb00b02db12940b6b5dda9a874
jessebrite <aboagye@protonmail.com>
2020-12-17 18:44:28 +0000

    Security login configured

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package tacos.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +15,19 @@ import javax.annotation.Resource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/design", "/orders")
+					.access("hasRole('ROLE_USER')")
+				.antMatchers("/", "/**")
+					.access("permitAll")
+			.and()
+				.formLogin()
+					.loginPage("/login");
+	}
 
 	@Resource(name = "userRepositoryUserDetailsService")
 	UserDetailsService userDetailsService;
