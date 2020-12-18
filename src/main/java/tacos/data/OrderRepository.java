9c2a7b2985e39cbf69853383ba3c50ffc023808b
jessebrite <aboagye@protonmail.com>
2020-12-18 22:19:25 +0000

    OrderRepository refactorerd

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -3,6 +3,4 @@ package tacos.data;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
 
-public interface OrderRepository extends CrudRepository<Order, Long> {
-	Order save (Order order);
-}
+public interface OrderRepository extends CrudRepository<Order, Long> {}
