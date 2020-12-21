d09df3f79ad264c7ec74cbd5d17d16a80d40e86f
jessebrite <aboagye@protonmail.com>
2020-12-21 21:55:00 +0000

    logout in SecurityConfig fixed

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -30,7 +30,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 				.formLogin()
 					.loginPage("/login")
-						.defaultSuccessUrl("/design", true);
+						.defaultSuccessUrl("/design", true)
+			.and()
+				.logout()
+					.logoutSuccessUrl("/");
 	}
 
 	@Resource(name = "userRepositoryUserDetailsService")
