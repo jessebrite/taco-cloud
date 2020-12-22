7ae64f55c1ed75cba437fbff0bd7e4cd9b49591d
jessebrite <aboagye@protonmail.com>
2020-12-22 22:16:14 +0000

    property name reverted to original

--- src/main/java/tacos/data/UserRepository.java
+++ src/main/java/tacos/data/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import tacos.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-	User findByUser(String username);
+	User findByUsername(String username);
 }
