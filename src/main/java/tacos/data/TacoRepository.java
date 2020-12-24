cfe2fcee3cd5c4fc722bba72ec4d5e06c475752d
jessebrite <aboagye@protonmail.com>
2020-12-24 22:20:16 +0000

    tacoUsername added in Taco Repo

--- src/main/java/tacos/data/TacoRepository.java
+++ src/main/java/tacos/data/TacoRepository.java
@@ -3,4 +3,6 @@ package tacos.data;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Taco;
 
-public interface TacoRepository extends CrudRepository<Taco, Long> {}
+public interface TacoRepository extends CrudRepository<Taco, Long> {
+	Taco findByTaconame (String tacoName);
+}
