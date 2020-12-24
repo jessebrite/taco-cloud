8cbb6252a51251eaf5a59300a301648f8a47e88d
jessebrite <aboagye@protonmail.com>
2020-12-24 22:21:47 +0000

    Reverted Taco Repo changes

--- src/main/java/tacos/data/TacoRepository.java
+++ src/main/java/tacos/data/TacoRepository.java
@@ -3,6 +3,4 @@ package tacos.data;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Taco;
 
-public interface TacoRepository extends CrudRepository<Taco, Long> {
-	Taco findByTaconame (String tacoName);
-}
+public interface TacoRepository extends CrudRepository<Taco, Long> {}
