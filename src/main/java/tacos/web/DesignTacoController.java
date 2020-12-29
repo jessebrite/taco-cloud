6c10702d676b7c6672f943ef8c507e19cf192b0b
jessebrite <aboagye@protonmail.com>
2020-12-29 11:50:59 +0000

    Reworking the project

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -67,7 +67,6 @@ public class DesignTacoController {
 
 		String username = principal.getName();
 		User user = userRepo.findByUsername(username);
-
 		model.addAttribute("user", user);
 
 		return "design";
