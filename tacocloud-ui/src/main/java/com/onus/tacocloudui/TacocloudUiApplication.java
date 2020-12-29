17d0ccb81d0e4b54bbd361a548b5fe4d08e6db9e
jessebrite <aboagye@protonmail.com>
2020-12-29 12:23:47 +0000

    tacocloud-domain reworked

---
+++ tacocloud-ui/src/main/java/com/onus/tacocloudui/TacocloudUiApplication.java
@@ -0,0 +1,13 @@
+package com.onus.tacocloudui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacocloudUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacocloudUiApplication.class, args);
+	}
+
+}
---
+++ tacocloud-ui/src/test/java/com/onus/tacocloudui/TacocloudUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onus.tacocloudui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacocloudUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
