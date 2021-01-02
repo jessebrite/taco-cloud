8378505288e164fbab58459b5b1c20b55485cd2c
jessebrite <aboagye@protonmail.com>
2021-01-02 10:26:37 +0000

    code refactor

--- tacocloud-domain/src/main/java/com/onus/tacoclouddomain/Order.java
+++ tacocloud-domain/src/main/java/com/onus/tacoclouddomain/Order.java
@@ -20,7 +20,7 @@ public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy= GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	private Date placedAt;
