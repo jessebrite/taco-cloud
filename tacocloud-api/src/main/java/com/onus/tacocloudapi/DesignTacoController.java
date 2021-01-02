d86e6e9697ce62592abd689026873afa6c6ff254
jessebrite <aboagye@protonmail.com>
2021-01-02 20:10:42 +0000

    code refactored "hypermedia" added for full-fledged RESTful API

--- tacocloud-api/src/main/java/com/onus/tacocloudapi/DesignTacoController.java
+++ tacocloud-api/src/main/java/com/onus/tacocloudapi/DesignTacoController.java
@@ -5,12 +5,18 @@ import com.onus.tacoclouddomain.Taco;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.EntityLinks;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(path="/design", produces="application/json")
@@ -26,10 +32,17 @@ public class DesignTacoController {
 	}
 
 	@GetMapping("/recent")
-	public Iterable<Taco> recentTacos() {
+	public CollectionModel<EntityModel<Taco>> recentTacos() {
 		PageRequest pageRequest = PageRequest.of(
 			0, 12, Sort.by("createdAt").descending());
-		return tacoRepo.findAll(pageRequest).getContent();
+
+		List<Taco> tacos = tacoRepo.findAll(pageRequest).getContent();
+		CollectionModel<EntityModel<Taco>> recentResources = CollectionModel.wrap(tacos);
+		recentResources.add(
+			linkTo(methodOn(DesignTacoController.class).recentTacos())
+			.withRel("recents"));
+
+		return recentResources;
 	}
 
 	@GetMapping("/{id}")
