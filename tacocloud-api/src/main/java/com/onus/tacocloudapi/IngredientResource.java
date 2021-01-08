e21b2c15f19a6818053dcadd4963b5b8d5763e7a
jessebrite <aboagye@protonmail.com>
2021-01-08 01:27:32 +0000

    IngredientResource implemented

--- tacocloud-api/src/main/java/com/onus/tacocloudapi/IngredientResource.java
+++ tacocloud-api/src/main/java/com/onus/tacocloudapi/IngredientResource.java
@@ -1,2 +1,20 @@
-package com.onus.tacocloudapi;public class IngredientResource {
+package com.onus.tacocloudapi;
+
+import com.onus.tacoclouddomain.Ingredient;
+import com.onus.tacoclouddomain.Ingredient.Type;
+import lombok.Getter;
+import org.springframework.hateoas.RepresentationModel;
+
+public class IngredientResource extends RepresentationModel {
+
+	@Getter
+	private final String name;
+
+	@Getter
+	private final Type type;
+
+	public IngredientResource(Ingredient ingredient) {
+		this.name = ingredient.getName();
+		this.type = ingredient.getType();
+	}
 }
