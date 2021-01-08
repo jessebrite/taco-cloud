02c53ac83a3bf588a26cac9e7e30863dc8b4b19f
jessebrite <aboagye@protonmail.com>
2021-01-08 01:51:04 +0000

    TacoResourceAssembler implemented

--- tacocloud-api/src/main/java/com/onus/tacocloudapi/TacoResourceAssembler.java
+++ tacocloud-api/src/main/java/com/onus/tacocloudapi/TacoResourceAssembler.java
@@ -1,2 +1,18 @@
-package com.onus.tacocloudapi;public class TacoResourceAssembler {
+package com.onus.tacocloudapi;
+
+import com.onus.tacoclouddomain.Taco;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+
+public class
+TacoResourceAssembler extends RepresentationModelAssemblerSupport<Taco, TacoResource> {
+
+	public TacoResourceAssembler() {
+		super(DesignTacoController.class, TacoResource.class);
+	}
+
+	@Override
+	public TacoResource toModel(Taco taco) {
+
+		return createModelWithId(taco.getId(), taco);
+	}
 }
