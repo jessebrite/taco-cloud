04d75b5a461c09e6fbaa9083edecc3ab7b4201f0
jessebrite <aboagye@protonmail.com>
2021-01-08 01:43:08 +0000

    IngredientResourceAssembler implemented

--- tacocloud-api/src/main/java/com/onus/tacocloudapi/IngredientResourceAssembler.java
+++ tacocloud-api/src/main/java/com/onus/tacocloudapi/IngredientResourceAssembler.java
@@ -1,2 +1,18 @@
-package com.onus.tacocloudapi;public class IngredientResourceAssembler {
+package com.onus.tacocloudapi;
+
+import com.onus.tacoclouddomain.Ingredient;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+
+public class IngredientResourceAssembler extends
+	RepresentationModelAssemblerSupport<Ingredient, IngredientResource> {
+
+	public IngredientResourceAssembler() {
+		super(IngredientController.class, IngredientResource.class);
+	}
+
+	@Override
+	public IngredientResource toModel(Ingredient ingredient) {
+
+		return createModelWithId(ingredient.getId(), ingredient);
+	}
 }
