c9501c5ee1102e709c12300062418325d955963c
jessebrite <aboagye@protonmail.com>
2021-01-08 01:40:51 +0000

    TacoResource implemented

--- tacocloud-api/src/main/java/com/onus/tacocloudapi/TacoResource.java
+++ tacocloud-api/src/main/java/com/onus/tacocloudapi/TacoResource.java
@@ -1,2 +1,36 @@
-package com.onus.tacocloudapi;public class TacoResource {
+package com.onus.tacocloudapi;
+
+import com.onus.tacoclouddomain.Taco;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.util.Date;
+
+public class TacoResource extends RepresentationModel<TacoResource> {
+
+	private static final IngredientResourceAssembler
+		ingredientAssembler = new IngredientResourceAssembler();
+
+	@Getter
+	private String name;
+
+	@Getter
+	private final Date createdAt;
+
+	@Getter
+	private final CollectionModel<IngredientResource> ingredients;
+
+	public TacoResource(Taco taco) {
+		this.name = taco.getName();
+		this.createdAt = taco.getCreatedAt();
+		this.ingredients = ingredientAssembler.toCollectionModel(taco.getIngredients());
+	}
+
+	@Autowired
+	public TacoResource(Date createdAt, CollectionModel<IngredientResource> ingredients) {
+		this.createdAt = createdAt;
+		this.ingredients = ingredients;
+	}
 }
